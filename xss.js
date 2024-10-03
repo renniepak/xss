f135a5bda806d92ca883f4b7348d613fe8ae2958
renniepak <renniepak@hotmail.com>
2024-10-03 18:54:12 +0200

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,5 @@
+alert(document.domain)
+
+// https://cdn.jsdelivr.net/ will allow you to proxy any file on github.com
+// https://cdn.jsdelivr.net/gh/<usernam>/<project>/<filename>
+// https://cdn.jsdelivr.net/gh/renniepak/xss/xss.js
