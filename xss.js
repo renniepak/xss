a8e495c6c8b88a855097b940d320996e50ec6be1
renniepak <renniepak@hotmail.com>
2024-10-03 19:49:45 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,5 @@
 alert(document.domain)
 
 // https://cdn.jsdelivr.net/ will allow you to proxy any file on github.com
-// https://cdn.jsdelivr.net/gh/<usernam>/<project>/<filename>
+// https://cdn.jsdelivr.net/gh/<username>/<project>/<filename>
 // https://cdn.jsdelivr.net/gh/renniepak/xss/xss.js
